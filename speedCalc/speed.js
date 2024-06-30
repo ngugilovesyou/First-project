2d9933a7f44c716f820b4f2f6f7ab496e2494414
Samuel <amsamgitau@gmail.com>
2024-06-30 23:45:59 +0300

    modify speed.js

--- speedCalc/speed.js
+++ speedCalc/speed.js
@@ -1,15 +1,14 @@
 const prompt = require('prompt-sync')();
 
 
-
 function carSpeed (){
 const speed = prompt('Input the speed of the car: ');
 const speedLimit= 70;
 
     if(speed < speedLimit){
         return "Ok"
-    }else {
-        let demeritPoint = Math.floor((speed - speedLimit) / 5);
+    }else { //takes the speed subtracts the speedlimit and divides by 5km/hr to get distance above speed limit
+        let demeritPoint = Math.floor((speed - speedLimit) / 5);//returns an integer
         if (demeritPoint>12) {
             return "License Suspended";
     
